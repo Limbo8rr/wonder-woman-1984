2de50b3a0d92e548e5b6def284016ec9b6599185
Limbo8rr <77548425+Limbo8rr@users.noreply.github.com>
2021-03-08 09:32:17 -0800

    Update main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,5 +1,12 @@
 scene.onOverlapTile(SpriteKind.Player, assets.tile`myTile4`, function (sprite, location) {
-    game.over(false)
+    if (Math.percentChance(20)) {
+        info.startCountdown(10)
+    } else if (Math.percentChance(30)) {
+        game.over(false)
+    } else if (Math.percentChance(70)) {
+        controller.moveSprite(wonderWoman)
+        controller.moveSprite(wonderwoman2)
+    }
 })
 scene.onOverlapTile(SpriteKind.Player, sprites.dungeon.collectibleBlueCrystal, function (sprite, location) {
     music.baDing.play()
@@ -11,7 +18,7 @@ scene.onOverlapTile(SpriteKind.Player, sprites.dungeon.chestClosed, function (sp
     info.changeScoreBy(randint(50, 500))
     tiles.setTileAt(location, sprites.dungeon.darkGroundCenter)
 })
-scene.onOverlapTile(SpriteKind.Player, assets.tile`tile2`, function (sprite, location) {
+scene.onOverlapTile(SpriteKind.Player, assets.tile`myTile5`, function (sprite, location) {
     game.over(true)
 })
 scene.onOverlapTile(SpriteKind.Player, sprites.dungeon.collectibleRedCrystal, function (sprite, location) {
@@ -23,7 +30,9 @@ scene.onOverlapTile(SpriteKind.Player, sprites.dungeon.collectibleInsignia, func
     tiles.setTileAt(location, sprites.dungeon.chestClosed)
     info.changeScoreBy(1000)
 })
-let wonderWoman = sprites.create(img`
+let wonderwoman2: Sprite = null
+let wonderWoman: Sprite = null
+wonderWoman = sprites.create(img`
     . . . . . d 2 2 2 d . . . . . . 
     . . . . . 5 2 5 2 5 . . . . . . 
     . . . . . d 8 8 8 d . . . . . . 
@@ -31,7 +40,7 @@ let wonderWoman = sprites.create(img`
     . . . . . . d . d . . . . . . . 
     . . . . . . 2 . 2 . . . . . . . 
     `, SpriteKind.Player)
-let wonderwoman2 = sprites.create(img`
+wonderwoman2 = sprites.create(img`
     . . . . . . . . . . . . . . . . 
     . . . . . f f 5 f f . . . . . . 
     . . . . f f 5 2 5 f f . . . . . 
